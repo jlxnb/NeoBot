3781d98fbd0e05578057945350f0857dcc7291e2
Aurelian2842 <aurelian2842@gmail.com>
2025-12-13 10:38:30 +0800

    fix: velocity event wasn't triggered & chat event params can't be gotten by js

--- common/src/main/java/dev/neovoxel/neobot/game/event/ChatEvent.java
+++ common/src/main/java/dev/neovoxel/neobot/game/event/ChatEvent.java
@@ -4,7 +4,7 @@ import dev.neovoxel.neobot.adapter.Player;
 import lombok.Getter;
 import org.graalvm.polyglot.HostAccess;
 
-@Getter
+@Getter(onMethod_ = {@HostAccess.Export})
 public abstract class ChatEvent {
     private final Player player;
     private final String message;
